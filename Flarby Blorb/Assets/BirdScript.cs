c62ad4225f23351e71b10635daa6aa6248def88e
majd bousaad <bousaadmajd@gmail.com>
2023-04-05 00:20:37 +0200

    Step 5
    Adding gameover screen

--- Flarby Blorb/Assets/BirdScript.cs
+++ Flarby Blorb/Assets/BirdScript.cs
@@ -6,20 +6,30 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public float flapStrength;
+    public LogicManagerScript logic;
+
+    private bool isAlive = true;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicManagerScript>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isAlive)
         {
             myRigidBody.velocity = Vector2.up * flapStrength;
 
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        isAlive = false;
     }
 }
--- Flarby Blorb/Assets/LogicManagerScript.cs
+++ Flarby Blorb/Assets/LogicManagerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicManagerScript : MonoBehaviour
 {
@@ -9,11 +10,23 @@ public class LogicManagerScript : MonoBehaviour
 
     public Text scoreText;
 
+    public GameObject gameOverScreen;
+
     [ContextMenu("Increase Score")]
     public void addScore()
     {
         playerScore += 1;
 
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
